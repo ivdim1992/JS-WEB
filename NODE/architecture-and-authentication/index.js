e1594ca6dd12f0a848ff90417b96e54b44b22b74
Ivaylo <ivdim1992@gmail.com>
2019-01-31 16:48:58 +0200

    initializing express-handlebars and prepaired app for work

--- NODE/architecture-and-authentication/index.js
+++ NODE/architecture-and-authentication/index.js
@@ -1,2 +1,25 @@
 const express = require('express');
+const port = 5000;
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+let env = process.env.NODE_ENV || 'development';
+
+
+let app = express();
+
+app.get('/', (req, res) => {
+
+    mongoose
+        .connect('mongodb://localhost:27017/architechtureTemplate')
+        .then(() => {
+            console.log('MongoDb ready');
+            res.send('Hello frmo EXpresSnod');
+        });
+            
+   
+
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
 
