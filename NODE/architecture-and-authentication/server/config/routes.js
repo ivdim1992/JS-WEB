002e6dc84259d30e083eabf9626fe5fa71169c70
Ivaylo <ivdim1992@gmail.com>
2019-02-01 12:05:14 +0200

    implementing authentication for different routes

--- NODE/architecture-and-authentication/server/config/routes.js
+++ NODE/architecture-and-authentication/server/config/routes.js
@@ -1,10 +1,10 @@
 const controllers = require('../controllers/index');
-
+const auth = require('../config/auth');
 
 module.exports = (app) => {
     app.get('/', controllers.home.index);
-    app.get('/about',controllers.home.about);
-    
+    app.get('/about',auth.isInRole('Admin'),controllers.home.about);
+
     app.get('/users/register',controllers.users.registerGet);
     app.post('/users/register',controllers.users.registerPost);
     app.get('/users/login',controllers.users.loginGet);
